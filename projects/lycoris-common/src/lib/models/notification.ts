71f6ec5bc2c80b199c20a072fe56dcd7f3670fc2
sdoubey <sebastien.doubey@gmail.com>
2022-02-04 15:30:04 +0100

    Add data in notification

--- projects/lycoris-common/src/lib/models/notification.ts
+++ projects/lycoris-common/src/lib/models/notification.ts
@@ -1,6 +1,7 @@
 import {NotificationAction} from './notification-action';
 
-export interface Notification<T> {
+export interface Notification<T, D> {
+  readonly data: D;
   readonly domain: T;
   readonly entityId: string;
   readonly action: NotificationAction;
